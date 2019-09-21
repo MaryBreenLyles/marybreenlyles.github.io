29d3f68cbabf4b0cc4bdaf0509b1b4eafa997d2a
Mary <mary.breenlyles@gmail.com>
2019-09-20 23:53:59 -0500

    Deploying build to master
    Added CNAME, gitignore, .git, and emptied build folder into the master branch for deployment. (github pages master branch deployment rule)

---
+++ precache-manifest.c0f2030bae29fedc5223f538e8c36cc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a88a43034bc8fa1ecb8ac8dbf3c43db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3305d6075b6be0ff3ae5",
+    "url": "/static/css/main.f06d9a16.chunk.css"
+  },
+  {
+    "revision": "25453f4afbe2baab1268",
+    "url": "/static/js/2.3c27f9ad.chunk.js"
+  },
+  {
+    "revision": "3305d6075b6be0ff3ae5",
+    "url": "/static/js/main.d325fc89.chunk.js"
+  },
+  {
+    "revision": "11110954aa50e9c9dea6",
+    "url": "/static/js/runtime-main.211a5a63.js"
+  }
+]);
